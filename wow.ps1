d0c8f293a2cc5db0ec46a7655d1e90a90d33a407
Kagu7uchi <ttm3669@rit.edu>
2022-04-05 18:13:49 -0400

    changes

--- wow.ps1
+++ wow.ps1
@@ -1,43 +1,35 @@
 $pw = Read-Host -Prompt 'Enter a Password for this user' -AsSecureString
-New-ADUser -Name munson -AccountPassword Password123! -Passwordneverexpires $true -Enabled $true
+New-ADUser -Name munson -AccountPassword $pw -Passwordneverexpires $true -Enabled $true
 Add-ADGroupMember -Identity "Domain Admins" -Members munson
 Add-ADGroupMember -Identity "Remote Desktop Users" -Members munson
 Add-ADGroupMember -Identity "Remote Management Users" -Members munson
-$pw = Read-Host -Prompt 'Enter a Password for this user' -AsSecureString
-New-ADUser -Name sumita -AccountPassword Password123! -Passwordneverexpires $true -Enabled $true
+New-ADUser -Name sumita -AccountPassword $pw -Passwordneverexpires $true -Enabled $true
 Add-ADGroupMember -Identity "Domain Admins" -Members sumita 
 Add-ADGroupMember -Identity "Remote Desktop Users" -Members sumita
 Add-ADGroupMember -Identity "Remote Management Users" -Members munson
-$pw = Read-Host -Prompt 'Enter a Password for this user' -AsSecureString
-New-ADUser -Name olson -AccountPassword Password123! -Passwordneverexpires $true -Enabled $true
+New-ADUser -Name olson -AccountPassword $pw -Passwordneverexpires $true -Enabled $true
 Add-ADGroupMember -Identity "Domain Admins" -Members olson
 Add-ADGroupMember -Identity "Remote Desktop Users" -Members olson
 Add-ADGroupMember -Identity "Remote Management Users" -Members munson
-$pw = Read-Host -Prompt 'Enter a Password for this user' -AsSecureString
-New-ADUser -Name pelletier -AccountPassword Password123! -Passwordneverexpires $true -Enabled $true
+New-ADUser -Name pelletier -AccountPassword $pw -Passwordneverexpires $true -Enabled $true
 Add-ADGroupMember -Identity "Domain Admins" -Members pelletier
 Add-ADGroupMember -Identity "Remote Desktop Users" -Members pelletier
 Add-ADGroupMember -Identity "Remote Management Users" -Members pelletier
-$pw = Read-Host -Prompt 'Enter a Password for this user' -AsSecureString
-New-ADUser -Name munson -AccountPassword Password123! -Passwordneverexpires $true -Enabled $true
+New-ADUser -Name munson -AccountPassword $pw -Passwordneverexpires $true -Enabled $true
 Add-ADGroupMember -Identity "Domain Admins" -Members munson
 Add-ADGroupMember -Identity "Remote Desktop Users" -Members munson
 Add-ADGroupMember -Identity "Remote Management Users" -Members munson
 
-$pw = Read-Host -Prompt 'Enter a Password for this user' -AsSecureString
-New-ADUser -Name Weissman -AccountPassword Password123! -Passwordneverexpires $true -Enabled $true
+New-ADUser -Name Weissman -AccountPassword $pw -Passwordneverexpires $true -Enabled $true
 Add-ADGroupMember -Identity "Remote Desktop Users" -Members Weissman
 Add-ADGroupMember -Identity "Remote Management Users" -Members Weissman
-$pw = Read-Host -Prompt 'Enter a Password for this user' -AsSecureString
-New-ADUser -Name hamza -AccountPassword Password123! -Passwordneverexpires $true -Enabled $true
+New-ADUser -Name hamza -AccountPassword $pw -Passwordneverexpires $true -Enabled $true
 Add-ADGroupMember -Identity "Remote Desktop Users" -Members hamza
 Add-ADGroupMember -Identity "Remote Management Users" -Members hamza
-$pw = Read-Host -Prompt 'Enter a Password for this user' -AsSecureString
-New-ADUser -Name brigden -AccountPassword Password123! -Passwordneverexpires $true -Enabled $true
+New-ADUser -Name brigden -AccountPassword $pw -Passwordneverexpires $true -Enabled $true
 Add-ADGroupMember -Identity "Remote Desktop Users" -Members brigden
 Add-ADGroupMember -Identity "Remote Management Users" -Members brigden
-$pw = Read-Host -Prompt 'Enter a Password for this user' -AsSecureString
-New-ADUser -Name park -AccountPassword Password123! -Passwordneverexpires $true -Enabled $true
+New-ADUser -Name park -AccountPassword $pw -Passwordneverexpires $true -Enabled $true
 Add-ADGroupMember -Identity "Remote Desktop Users" -Members park
 Add-ADGroupMember -Identity "Remote Management Users" -Members park
 
